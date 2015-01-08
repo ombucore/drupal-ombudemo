b3c0004058975a85bf55051f75c4af173145df0c
Jon Duell <jon@jonduell.com>
2015-01-08 12:00:57 -0800

    Fixes bug with select2 overriding Drupal multi widgets - fixes #10302

--- public/sites/all/modules/contrib/select2/js/select2_integration.js
+++ public/sites/all/modules/contrib/select2/js/select2_integration.js
@@ -121,7 +121,7 @@
       return this.replace(/"$/, '').replace(/^"/, '');
     }
 
-    return this;
+    return this.toString();
   };
 
   $.fn.atachSelect2 = function () {
@@ -238,9 +238,7 @@
               if (options.placeholder == undefined) {
                 options.placeholder = $('option[value=""]', $element).text();
               }
-
-              $('option[value=""]', $element).html('').removeAttr('selected');
-
+              $('option[value=""]', $element).html('');
             }
 
           }
