17f75da17d9e805bce0ae1d526bf6d82eb987852
Adam Page <adamfpage@gmail.com>
2016-01-12 10:50:20 -0800

    Refs #14823: Refactored margin and padding metrics for tile sections.

--- public/sites/all/themes/bridges_theme/templates/region--footer.tpl.php
+++ public/sites/all/themes/bridges_theme/templates/region--footer.tpl.php
@@ -27,7 +27,7 @@
  */
 ?>
 <?php if ($content): ?>
-  <div data-type="region" data-name="section-arch" class="<?php print $classes ?>" id="<?php print $id . '-arch' ?>"<?php print $attributes ?>>
+  <div data-type="region" data-name="footer-arch" class="<?php print $classes ?>" id="<?php print $id . '-arch' ?>"<?php print $attributes ?>>
     <div class="container">
       <div class="arch">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 320 20" enable-background="new 0 0 320 20" xml:space="preserve">
